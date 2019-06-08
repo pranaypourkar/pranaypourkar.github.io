23af0e0a9b5a065a9ced43db11105ef63ad43336
pranaypourkar <pranay.pourkar@gmail.com>
2019-06-08 20:52:03 +0530

    Initial commit

--- js/web.js
+++ js/web.js
@@ -1,42 +1,42 @@
 var database = [];
 
 		//mobile
-  		database.push({Product_ID:"1",Price:13139,img_location:"images/Mobile1.jpg",description:"Xiaomi Redmi Note 6 Pro",page_location:"F:/course/CGI_web/web/categories/Mobiles.html"}); 
-  		database.push({Product_ID:"2",Price:18100,img_location:"images/Mobile2.jpg",description:"Nokia 7.1 Plus",page_location:"F:/course/CGI_web/web/categories/Mobiles.html"});
-  		database.push({Product_ID:"3",Price:18887,img_location:"images/Mobile3.jpg",description:"Samsung Galaxy A7",page_location:"F:/course/CGI_web/web/categories/Mobiles.html"}); 
-  		database.push({Product_ID:"4",Price:13999,img_location:"images/Mobile4.jpg",description:"Honor 10 Lite",page_location:"F:/course/CGI_web/web/categories/Mobiles.html"});
+  		database.push({Product_ID:"1",Price:13139,img_location:"images/Mobile1.jpg",description:"Xiaomi Redmi Note 6 Pro",page_location:"categories/Mobiles.html"}); 
+  		database.push({Product_ID:"2",Price:18100,img_location:"images/Mobile2.jpg",description:"Nokia 7.1 Plus",page_location:"categories/Mobiles.html"});
+  		database.push({Product_ID:"3",Price:18887,img_location:"images/Mobile3.jpg",description:"Samsung Galaxy A7",page_location:"categories/Mobiles.html"}); 
+  		database.push({Product_ID:"4",Price:13999,img_location:"images/Mobile4.jpg",description:"Honor 10 Lite",page_location:"categories/Mobiles.html"});
   		//bags
-  		database.push({Product_ID:"5",Price:499,img_location:"images/Bag1.jpg",description:"Pole Star",page_location:"F:/course/CGI_web/web/categories/Bags.html"}); 
-  		database.push({Product_ID:"6",Price:809,img_location:"images/Bag2.jpg",description:"Fastrack",page_location:"F:/course/CGI_web/web/categories/Bags.html"});
-  		database.push({Product_ID:"7",Price:474,img_location:"images/Bag3.jpg",description:"Gear",page_location:"F:/course/CGI_web/web/categories/Bags.html"}); 
-  		database.push({Product_ID:"8",Price:501,img_location:"images/Bag4.jpg",description:"Sassie",page_location:"F:/course/CGI_web/web/categories/Bags.html"});
+  		database.push({Product_ID:"5",Price:499,img_location:"images/Bag1.jpg",description:"Pole Star",page_location:"categories/Bags.html"}); 
+  		database.push({Product_ID:"6",Price:809,img_location:"images/Bag2.jpg",description:"Fastrack",page_location:"categories/Bags.html"});
+  		database.push({Product_ID:"7",Price:474,img_location:"images/Bag3.jpg",description:"Gear",page_location:"categories/Bags.html"}); 
+  		database.push({Product_ID:"8",Price:501,img_location:"images/Bag4.jpg",description:"Sassie",page_location:"categories/Bags.html"});
 
   		//sports
-  		database.push({Product_ID:"9",Price:875,img_location:"images/sports1.jpg",description:"Yonex",page_location:"F:/course/CGI_web/web/categories/Sports.html"}); 
-  		database.push({Product_ID:"10",Price:500,img_location:"images/sports2.jpg",description:"Nivia",page_location:"F:/course/CGI_web/web/categories/Sports.html"});
-  		database.push({Product_ID:"11",Price:7000,img_location:"images/sports3.jpg",description:"Lifeline",page_location:"F:/course/CGI_web/web/categories/Sports.html"}); 
-  		database.push({Product_ID:"12",Price:1000,img_location:"images/sports4.jpg",description:"RUBX",page_location:"F:/course/CGI_web/web/categories/Sports.html"});
+  		database.push({Product_ID:"9",Price:875,img_location:"images/sports1.jpg",description:"Yonex",page_location:"categories/Sports.html"}); 
+  		database.push({Product_ID:"10",Price:500,img_location:"images/sports2.jpg",description:"Nivia",page_location:"categories/Sports.html"});
+  		database.push({Product_ID:"11",Price:7000,img_location:"images/sports3.jpg",description:"Lifeline",page_location:"categories/Sports.html"}); 
+  		database.push({Product_ID:"12",Price:1000,img_location:"images/sports4.jpg",description:"RUBX",page_location:"categories/Sports.html"});
 
   		//music cd's
-  		database.push({Product_ID:"13",Price:175,img_location:"images/CD1.jpg",description:"RDB The Musical Genious",page_location:"F:/course/CGI_web/web/categories/Music_CDs.html"}); 
-  		database.push({Product_ID:"14",Price:250,img_location:"images/CD2.jpg",description:"Soulful Voice",page_location:"F:/course/CGI_web/web/categories/Music_CDs.html"});
-  		database.push({Product_ID:"15",Price:200,img_location:"images/CD3.jpg",description:"Always Your'S Kumar Sanu",page_location:"F:/course/CGI_web/web/categories/Music_CDs.html"}); 
-  		database.push({Product_ID:"16",Price:300,img_location:"images/CD4.jpg",description:"Best of Sonu Nigam",page_location:"F:/course/CGI_web/web/categories/Music_CDs.html"});
+  		database.push({Product_ID:"13",Price:175,img_location:"images/CD1.jpg",description:"RDB The Musical Genious",page_location:"categories/Music_CDs.html"}); 
+  		database.push({Product_ID:"14",Price:250,img_location:"images/CD2.jpg",description:"Soulful Voice",page_location:"categories/Music_CDs.html"});
+  		database.push({Product_ID:"15",Price:200,img_location:"images/CD3.jpg",description:"Always Your'S Kumar Sanu",page_location:"categories/Music_CDs.html"}); 
+  		database.push({Product_ID:"16",Price:300,img_location:"images/CD4.jpg",description:"Best of Sonu Nigam",page_location:"categories/Music_CDs.html"});
 
   		//dresses
-  		database.push({Product_ID:"17",Price:399,img_location:"images/Dress1.jpg",description:"Cenizas Men's T-shirt",page_location:"F:/course/CGI_web/web/categories/Dresses.html"}); 
-  		database.push({Product_ID:"18",Price:299,img_location:"images/Dress2.jpg",description:"Cub McPaws T-shirt",page_location:"F:/course/CGI_web/web/categories/Dresses.html"});
-  		database.push({Product_ID:"19",Price:309,img_location:"images/Dress3.jpg",description:"Cub McPaws T-shirt",page_location:"F:/course/CGI_web/web/categories/Dresses.html"}); 
-  		database.push({Product_ID:"20",Price:300,img_location:"images/Dress4.jpg",description:"Cub McPaws T-shirt",page_location:"F:/course/CGI_web/web/categories/Dresses.html"});
+  		database.push({Product_ID:"17",Price:399,img_location:"images/Dress1.jpg",description:"Cenizas Men's T-shirt",page_location:"categories/Dresses.html"}); 
+  		database.push({Product_ID:"18",Price:299,img_location:"images/Dress2.jpg",description:"Cub McPaws T-shirt",page_location:"categories/Dresses.html"});
+  		database.push({Product_ID:"19",Price:309,img_location:"images/Dress3.jpg",description:"Cub McPaws T-shirt",page_location:"categories/Dresses.html"}); 
+  		database.push({Product_ID:"20",Price:300,img_location:"images/Dress4.jpg",description:"Cub McPaws T-shirt",page_location:"categories/Dresses.html"});
 
   		//electronics
-  		database.push({Product_ID:"21",Price:9990,img_location:"images/electronics1.jpg",description:"Panasonic HD LED TV",page_location:"F:/course/CGI_web/web/categories/Electronics.html"}); 
-  		database.push({Product_ID:"22",Price:30384,img_location:"images/electronics2.jpg",description:"Dell Inspiron Laptop",page_location:"F:/course/CGI_web/web/categories/Electronics.html"});
-  		database.push({Product_ID:"23",Price:65800,img_location:"images/electronics3.jpg",description:"Nikon DSLR Camera",page_location:"F:/course/CGI_web/web/categories/Electronics.html"}); 
-  		database.push({Product_ID:"24",Price:5999,img_location:"images/electronics4.jpg",description:"HP DeskJet Printer",page_location:"F:/course/CGI_web/web/categories/Electronics.html"});
+  		database.push({Product_ID:"21",Price:9990,img_location:"images/electronics1.jpg",description:"Panasonic HD LED TV",page_location:"categories/Electronics.html"}); 
+  		database.push({Product_ID:"22",Price:30384,img_location:"images/electronics2.jpg",description:"Dell Inspiron Laptop",page_location:"categories/Electronics.html"});
+  		database.push({Product_ID:"23",Price:65800,img_location:"images/electronics3.jpg",description:"Nikon DSLR Camera",page_location:"categories/Electronics.html"}); 
+  		database.push({Product_ID:"24",Price:5999,img_location:"images/electronics4.jpg",description:"HP DeskJet Printer",page_location:"categories/Electronics.html"});
 
   		//shoes
-  		database.push({Product_ID:"25",Price:1875,img_location:"images/shoe1.jpg",description:"Red Tape",page_location:"F:/course/CGI_web/web/categories/Shoes.html"}); 
-  		database.push({Product_ID:"26",Price:2500,img_location:"images/shoe2.jpg",description:"Sparx",page_location:"F:/course/CGI_web/web/categories/Shoes.html"});
-  		database.push({Product_ID:"27",Price:2000,img_location:"images/shoe3.jpg",description:"Lancer",page_location:"F:/course/CGI_web/web/categories/Shoes.html"}); 
-  		database.push({Product_ID:"28",Price:3000,img_location:"images/shoe4.jpg",description:"Asian",page_location:"F:/course/CGI_web/web/categories/Shoes.html"});
+  		database.push({Product_ID:"25",Price:1875,img_location:"images/shoe1.jpg",description:"Red Tape",page_location:"categories/Shoes.html"}); 
+  		database.push({Product_ID:"26",Price:2500,img_location:"images/shoe2.jpg",description:"Sparx",page_location:"categories/Shoes.html"});
+  		database.push({Product_ID:"27",Price:2000,img_location:"images/shoe3.jpg",description:"Lancer",page_location:"categories/Shoes.html"}); 
+  		database.push({Product_ID:"28",Price:3000,img_location:"images/shoe4.jpg",description:"Asian",page_location:"categories/Shoes.html"});
